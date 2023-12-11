4f6a918c77e016e72c91a0d0f8a62d7b863066c8
Jesse Kearns <JKearns@rightpoint.com>
2023-12-11 07:48:06 -0600

    Day 11 A

--- 11/solution.py
+++ 11/solution.py
@@ -4,10 +4,61 @@ inputfile = Path(__file__).parent / "input.txt"
 with open(inputfile) as f:
     lines = f.readlines()
 
-sum = 0
+grid = []
 
+# Expand vertically
 for line in lines:
-    # TODO
-    sum = sum + 1
+    galaxyFound = False
+    for c in line:
+        if c =='#':
+            galaxyFound = True
 
-print('Part 1: ' + str(sum))
+    grid.append(line)
+    if(not galaxyFound):
+        grid.append(line)
+
+# Expand horizontally
+horizontalExpansionIndices = []
+for x in range(0, len(grid[0]) - 1):
+    galaxyFound = False
+    for y in range(0, len(grid)):
+        if grid[y][x] == '#':
+            galaxyFound = True
+    if(not galaxyFound):
+        horizontalExpansionIndices.append(x)
+
+newGrid = []
+
+horizontalExpansionIndices.reverse() # Start from the right edge to avoid index math as we expand
+for row in grid:
+    for i in horizontalExpansionIndices:
+        row = row[:i] + '.' + row[i:]
+    newGrid.append(row)
+
+grid = newGrid
+
+xMax = len(grid[0]) - 1
+yMax = len(grid)
+galaxies = []
+for x in range(0, xMax):
+    for y in range(0, yMax):
+        if grid[y][x] == '#':
+            galaxies.append((x, y))
+
+distances = dict()
+for a in range (0, len(galaxies) - 1):
+    for b in range(a + 1, len(galaxies)):
+        gA = galaxies[a]
+        gB = galaxies[b]
+        # Sort for redundancy checking: A should be less than B
+        if gA[0] > gB[0] or (gA[0] == gB[0] and gA[1] > gB[1]):
+            temp = gA
+            gA = gB
+            gB = temp
+        distances[(gA, gB)] = abs(gA[0] - gB[0]) + abs(gA[1] - gB[1])
+
+sum = 0
+for key in distances:
+    sum = sum + distances[key]
+
+print('Sum of lengths: ' + str(sum))
