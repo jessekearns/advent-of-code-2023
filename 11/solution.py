dd0648fe301fe8bd44fb67225c34bdb90a209c0e
Jesse Kearns <JKearns@rightpoint.com>
2023-12-11 13:42:41 -0600

    Part 2

--- 11/solution.py
+++ 11/solution.py
@@ -2,20 +2,21 @@ from pathlib import Path
 
 inputfile = Path(__file__).parent / "input.txt"
 with open(inputfile) as f:
-    lines = f.readlines()
+    grid = f.readlines()
 
-grid = []
+expansionFactor = 999999 # 1 for part 1, (1M-1) for part 2
 
+y = 0
+verticalExpansionIndices = []
 # Expand vertically
-for line in lines:
+for row in grid:
     galaxyFound = False
-    for c in line:
+    for c in row:
         if c =='#':
             galaxyFound = True
-
-    grid.append(line)
-    if(not galaxyFound):
-        grid.append(line)
+    if not galaxyFound:
+        verticalExpansionIndices.append(y)
+    y = y + 1
 
 # Expand horizontally
 horizontalExpansionIndices = []
@@ -24,18 +25,8 @@ for x in range(0, len(grid[0]) - 1):
     for y in range(0, len(grid)):
         if grid[y][x] == '#':
             galaxyFound = True
-    if(not galaxyFound):
+    if not galaxyFound:
         horizontalExpansionIndices.append(x)
-
-newGrid = []
-
-horizontalExpansionIndices.reverse() # Start from the right edge to avoid index math as we expand
-for row in grid:
-    for i in horizontalExpansionIndices:
-        row = row[:i] + '.' + row[i:]
-    newGrid.append(row)
-
-grid = newGrid
 
 xMax = len(grid[0]) - 1
 yMax = len(grid)
@@ -44,21 +35,33 @@ for x in range(0, xMax):
     for y in range(0, yMax):
         if grid[y][x] == '#':
             galaxies.append((x, y))
+            # print((x, y))
+
+adjustedGalaxies = []
+for galaxy in galaxies:
+    newX = galaxy[0]
+    for horizontalExp in horizontalExpansionIndices:
+        if galaxy[0] > horizontalExp:
+            newX = newX + expansionFactor
+
+    newY = galaxy[1]
+    for verticalExp in verticalExpansionIndices:
+        if galaxy[1] > verticalExp:
+            newY = newY + expansionFactor
+
+    adjustedGalaxies.append((newX, newY))
 
 distances = dict()
-for a in range (0, len(galaxies) - 1):
-    for b in range(a + 1, len(galaxies)):
-        gA = galaxies[a]
-        gB = galaxies[b]
-        # Sort for redundancy checking: A should be less than B
-        if gA[0] > gB[0] or (gA[0] == gB[0] and gA[1] > gB[1]):
-            temp = gA
-            gA = gB
-            gB = temp
-        distances[(gA, gB)] = abs(gA[0] - gB[0]) + abs(gA[1] - gB[1])
+for a in range (0, len(adjustedGalaxies) - 1):
+    for b in range(a + 1, len(adjustedGalaxies)):
+        gA = adjustedGalaxies[a]
+        gB = adjustedGalaxies[b]
+        distance = abs(gA[0] - gB[0]) + abs(gA[1] - gB[1])
+        distances[(gA, gB)] = distance
 
 sum = 0
 for key in distances:
+    # print(str(key) + ': ' + str(distances[key]))
     sum = sum + distances[key]
 
 print('Sum of lengths: ' + str(sum))
