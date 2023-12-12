5524d247b11f20131490b08b5cd9d7e1d44562c5
Jesse Kearns <JKearns@rightpoint.com>
2023-12-12 12:41:45 -0600

    Day 12 Part 1 naive approach

--- 12/solution.py
+++ 12/solution.py
@@ -1,13 +1,49 @@
 from pathlib import Path
 
+def CountValidPatterns(input, numbers):
+    if '?' in input:
+        for i in range(0, len(input)):
+             if input[i] == '?':
+                  return CountValidPatterns(input[:i] + '.' + input[i + 1:], numbers) + CountValidPatterns(input[:i] + '#' + input[i + 1:], numbers)
+    else:
+        currentCount = 0
+        resultingCounts = []
+        for c in input:
+            if c == '#':
+                currentCount = currentCount + 1
+            else:
+                if currentCount > 0:
+                    resultingCounts.append(currentCount)
+                currentCount = 0
+        if currentCount > 0:
+                    resultingCounts.append(currentCount)
+
+        if resultingCounts == numbers:
+             return 1
+        else:
+             return 0
+
 inputfile = Path(__file__).parent / "input.txt"
 with open(inputfile) as f:
     lines = f.readlines()
 
+isFolded = True
 sum = 0
 
 for line in lines:
-    # TODO
-    sum = sum + 1
+    segments = line.split(' ')
+    nums = []
+    for numString in segments[1].replace('\n', '').split(','):
+         nums.append(int(numString))
 
-print('Part 1: ' + str(sum))
+    input = segments[0]
+    if isFolded:
+         input = segments[0] + '?' + segments[0] + '?' + segments[0] + '?' + segments[0] + '?' + segments[0]
+         newNums = nums + nums + nums + nums + nums
+         nums = newNums
+    configurations = CountValidPatterns(input, nums)
+
+    sum = sum + configurations
+    print(input + ' ' + str(nums) + ' ' + str(configurations))
+
+print('Sum: ' + str(sum))
